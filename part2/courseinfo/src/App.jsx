a912862c779dd1f6636947c9a1bd226279ca6bef
brennandoubt <brennandoubt@gmail.com>
2023-12-17 04:29:48 -0600

    courseinfo step6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,51 @@
+
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Header = ({ text }) => <h2>{text}</h2>
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
