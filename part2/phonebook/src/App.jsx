316f63dedfc7ebb7a34956a4c036189a4757527f
brennandoubt <brennandoubt@gmail.com>
2023-12-17 08:23:06 -0600

    phonebook step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,37 +1,44 @@
 import { useState } from "react"
 
-const Person = ({ name }) => <div>{name}</div>
+const Person = ({ name, number }) => <div>{name} {number}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas', number: '040-1234567'}
   ])
 
   // for controlling form input element
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   // executes when clicking form button
   const addPerson = (event) => {
     event.preventDefault()
-
+    
+    // check if name already in phonebook
     const isNotDuplicate = persons.every(person => (person.name != newName))
-    console.log(isNotDuplicate);
-
     if (!isNotDuplicate) {
       alert(`${newName} is already added to phonebook`)  // browser pop-up alert w/ template string
       return
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
+
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -42,11 +49,14 @@ const App = () => {
           name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.name} name={person.name} />)}
+      {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 }
