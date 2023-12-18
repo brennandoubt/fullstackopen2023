1a0a77fb80e8524052bba453b275d668dce13ece
brennandoubt <brennandoubt@gmail.com>
2023-12-18 15:49:51 -0600

    Refactor countries app

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -5,12 +5,20 @@ import axios from 'axios'
 const api_key = import.meta.env.VITE_WS_KEY
 console.log(api_key)
 
+const Button = ({ id, text, handleClick }) => <button id={id} onClick={handleClick}>{text}</button>
+
+const CountryLine = ({ name, handleShowClick }) => {
+  return (
+    <div>
+      {name} <Button id={name} text='show' handleClick={handleShowClick} />
+    </div>
+  )
+}
 
 const CountryData = ({ name, capital, area, languages, flag, latlng }) => {
   //console.log(`${name}'s coords`, latlng)
   let units = 'metric'
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&units=${units}&appid=${api_key}`
-
 
   return (
     <div>
@@ -32,6 +40,7 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [searchedCountry, setSearchedCountry] = useState(``)
   const [showCountry, setShowCountry] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
 
   // RESTful API to get country data
   const baseUrl = `https://studies.cs.helsinki.fi/restcountries/`
@@ -40,68 +49,53 @@ const App = () => {
     console.log('effect')
 
     axios
-      .get(showCountry ? (baseUrl + `api/name/${showCountry.name.common}`) : (baseUrl + `api/all`))
+      .get((baseUrl + `api/all`))
       .then(response => {
         setCountries(response.data)
-        //console.log('response data:', response.data)
+        console.log('response data:', response.data)
       })
-  }, [showCountry])
+    
+  }, [])
 
   // filter countries shown by search field input
-  let countriesToShow = (countries.length > 1)
-    ? countries.filter(country => country.name.common.toLowerCase().includes(searchedCountry.toLowerCase()))
-    : countries
+  let countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(searchedCountry.toLowerCase()))
+
   const handleFilterChange = (event) => {
+    setShowCountry(null)
     event.preventDefault()
-    console.log(event.target.value, showCountry, 'status')
+    console.log(event.target.value, showCountry, '<-- status')
 
     const updatedCountryName = event.target.value
-    countriesToShow = showCountry
-      ? setShowCountry(null)
-      : (countries.length > 1)
-        ? countries.filter(country => country.name.common.toLowerCase().includes(updatedCountryName.toLowerCase()))
-        : countries
-    console.log(countriesToShow, countries)
+    countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(updatedCountryName.toLowerCase()))
+    console.log(`countries to show:`, countriesToShow, countries)
+
+    if (countriesToShow.length === 1) {
+      setShowCountry(countriesToShow[0])
+    }
+
     setSearchedCountry(event.target.value)
   }
 
-  const handleCountryView = (event) => {
+  const handleShowCountry = (event) => {
     event.preventDefault()
+    //console.log(event.target, event.target.id)
 
-    console.log(event.target.value, event.target, event.target.id)
-    const country = countries.find(c => c.name.official === event.target.id)
-    console.log(country)
+    const country = countries.find(c => c.name.common === event.target.id)
+    console.log(`country to show:`, country)
 
     setShowCountry(country)
   }
 
-  let listedCountries = []
-
   // limits countries listed to 10, otherwise asks user to be more specific
-  listedCountries = (countriesToShow.length > 10)
-  ? <div>Too many matches, specify another filter</div>
-  : (countriesToShow.length > 1 && !showCountry)
-    ? countriesToShow.map(country => <div key={country.name.official}>{country.name.common} <button id={country.name.official} onClick={handleCountryView}>show</button></div>)
-    : []
-  
-  
+  let listedCountries = (countriesToShow.length > 10)
+    ? <div>Too many matches, specify another filter</div>
+    : countriesToShow.map(country => <CountryLine key={country.name.official} name={country.name.common} handleShowClick={handleShowCountry} />)
   
  
   return (
     <div>
       <p>find countries <input value={searchedCountry} onChange={handleFilterChange} /></p>
-      {(countriesToShow.length === 1 && !showCountry) // case for showing country on filtered search
-        ? <CountryData 
-            name={countriesToShow[0].name.common}
-            capital={countriesToShow[0].capital}
-            area={countriesToShow[0].area}
-            languages={[countriesToShow[0].languages].map(ls => (Object.values(ls).map(l => <li key={l}>{l}</li>)))}
-            flag={countriesToShow[0].flag}
-            latlng={countriesToShow[0].capitalInfo.latlng}
-          />
-        : listedCountries
-      }
-      {(showCountry) // case for showing country on button click
+      {(showCountry) // case for showing country on button click or filtered search
         ? <CountryData 
             name={showCountry.name.common}
             capital={showCountry.capital}
@@ -110,7 +104,7 @@ const App = () => {
             flag={showCountry.flag}
             latlng={showCountry.capitalInfo.latlng}
           />
-        : []
+        : listedCountries
       }
     </div>
   )
