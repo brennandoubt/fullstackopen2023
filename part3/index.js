95d4b409ca0f3483009369603b69fc1180eff9dc
brennandoubt <brennandoubt@gmail.com>
2023-12-18 22:13:22 -0600

    Part 3a

--- part3/index.js
+++ part3/index.js
@@ -6,9 +6,17 @@
  * (npm install express) makes express a project dependency
  * (npm update) updates dependencies
  * (npm install) installs dependencies for separate device
+ * 
+ * Nodemon automatically restarts node app
+ * when file changes are made
+ * 
+ * Express helps make a REST interface
+ * for app
  */
-const http = require('http') // import Node's built-in web server module
 
+// import express function to create an express app
+const express = require('express')
+const app = express()
 
 let notes = [
    {
@@ -28,12 +36,37 @@ let notes = [
    }
 ]
 
-const app = http.createServer((request, response) => {
-   response.writeHead(200, { 'Content-Type': 'application/json' }) // respond to http request
-   response.end(JSON.stringify(notes)) // set content on site
+// define route to app to handle http get requests made to app's /root path
+app.get('/', (request, response) => {
+   response.send('<h1>Hello World!</h1>') // make server respond to http request
 })
 
-// bind web server to listen to http requests sent to port 3001 (http://localhost:3001)
+// define route to app to handle http get requests made to app's /api/notes path
+app.get('/api/notes', (request, response) => {
+   response.json(notes) // respond with notes array passed as a JSON formatted string
+})
+
+// implement route to handle http get requests of form /api/notes/x
+app.get('/api/notes/:id', (request, response) => {
+   const id = Number(request.params.id)
+   const note = notes.find(note => note.id === id)
+
+   if (note) {
+      response.json(note)
+   } else {
+      response.status(404).end() // change to status 404 if note not found
+   }
+})
+
+// implement route to delete resources: HTTP DELETE request
+app.delete('/api/notes/:id', (request, response) => {
+   const id = Number(request.params.id)
+   notes = notes.filter(note => note.id !== id)
+
+   response.status(204).end()
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`)
+})
