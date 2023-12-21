f3114f7ee95c3aa50685fe69f86b94f094425277
brennandoubt <brennandoubt@gmail.com>
2023-12-21 06:10:25 -0600

    add scripts for web deployment

--- part2/vite.config.js
+++ part2/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // add frontend target to send requests to correct backend address 
+  server: {
+    // React dev environment now works as a proxy
+    proxy: {
+      // redirect requests to '/api...' paths to the server
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
+  },
+  // ^ frontend now works with server both in development and production mode
 })
