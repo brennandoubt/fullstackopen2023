ae49316fd14828f43d6c68312639eb109f50336a
brennandoubt <brennandoubt@gmail.com>
2023-12-21 19:13:45 -0600

    Part 3c: update routes using note model created with mongoose

--- part3/index.js
+++ part3/index.js
@@ -47,21 +47,18 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
    const body = request.body
 
-   if (!body.content) {
-      return response.status(400).json({ // return here to keep bad note from being saved to app
-         error: 'content missing'
-      })
+   if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
    }
 
-   const note = {
+   const note = new Note({
       content: body.content,
       important: body.important || false,
-      id: generateId()
-   }
+   })
 
-   notes = notes.concat(note)
-
-   response.json(note)
+   note.save().then(savedNote => {
+      response.json(savedNote)
+   })
 })
 
 let notes = [
@@ -90,6 +87,7 @@ app.get('/', (request, response) => {
 // define route to app to handle http get requests made to app's /api/notes path
 app.get('/api/notes', (request, response) => {
    Note.find({}).then(notes => {
+      console.log(`response: ${notes}`)
       response.json(notes)
    })
    //response.json(notes) // respond with notes array passed as a JSON formatted string
@@ -97,14 +95,19 @@ app.get('/api/notes', (request, response) => {
 
 // implement route to handle http get requests of form /api/notes/x
 app.get('/api/notes/:id', (request, response) => {
-   const id = Number(request.params.id)
-   const note = notes.find(note => note.id === id)
+   // const id = Number(request.params.id)
+   // const note = notes.find(note => note.id === id)
 
-   if (note) {
+   // if (note) {
+   //    response.json(note)
+   // } else {
+   //    response.status(404).end() // change to status 404 if note not found
+   // }
+
+   Note.findById(request.params.id).then(note => {
+      console.log(`response: ${note}`)
       response.json(note)
-   } else {
-      response.status(404).end() // change to status 404 if note not found
-   }
+   })
 })
 
 // implement route to delete resources: HTTP DELETE request
