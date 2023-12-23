3d23393eb309df415d1cdc849d77a4c78b264c5a
brennandoubt <brennandoubt@gmail.com>
2023-12-23 16:16:51 -0600

    4.8 blog list tests step1

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,34 @@
+/**
+ * File to handle testing API for
+ * blog list app
+ */
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const testHelper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+// reset database prior to each test
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = testHelper.initialBlogs.map(blog => new Blog(blog))
+  const promises = blogObjects.map(blog => blog.save())
+  await Promise.all(promises)
+})
+
+// verify correct amount of blog posts in JSON format are returned from http get request
+test('all blogs are returned as JSON', async () => {
+  // check response is in JSON format
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  // check response has correct number of blogs
+  expect(response.body).toHaveLength(testHelper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
--- part4/utils/logger.js
+++ part4/utils/logger.js
@@ -4,13 +4,17 @@
  * and console.error
  */
 const info = (...params) => {
-   console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-   console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 module.exports = {
-   info, error
+  info, error
 }
