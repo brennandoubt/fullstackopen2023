82ff85150f205b80394c572ba632e2ef6877ae00
brennandoubt <brennandoubt@gmail.com>
2023-12-23 07:24:04 -0600

    4.4 helper functions and unit tests step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,6 +7,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+// return total sum of likes in all blog posts
+const totalLikes = (blogs) => {
+  const sumOfLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return sumOfLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
